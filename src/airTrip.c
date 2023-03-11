fa3f79f456828604b4c4746a48d56d992622e180
Luz Alba Posse <luzalbaposse@gmail.com>
2023-03-11 12:15:49 -0300

    Update airTrip.c

--- src/airTrip.c
+++ src/airTrip.c
@@ -115,6 +115,25 @@ struct airTrip* airTripNew(char* plane) {
 }
 
 void airTripAddLast(struct airTrip* trip, char* name, float longitude, float latitude) {
+    /*
+    Consigna: Agrega un nuevo airport al final de la lista de paradas de un trip. 
+    Para esto debe recorrer toda la lista hasta encontrar el último lugar donde agregar 
+    el nuevo nodo. La nueva estructura de airport debe ser completada con los campos 
+    pasados por parámetro.
+
+    Desarrollo:
+
+    El algoritmo que pensamos para esta función es el siguiente:
+    1. Reservamos memoria para el nuevo aeropuerto.
+    2. Copiamos el nombre del aeropuerto en el nuevo aeropuerto.
+    3. Copiamos la longitud y latitud del aeropuerto en el nuevo aeropuerto.
+    4. Agregamos el nuevo aeropuerto al final de la lista de aeropuertos.
+    5. Calculamos la longitud total del viaje.
+    6. Retornamos el puntero al nuevo aeropuerto.
+
+    Requiere: trip != NULL && name != NULL
+    Devuelve: el nuevo aeropuerto agregado.
+    */
 
     struct airport* actual = trip->first;
     while (actual->next != NULL){
@@ -130,8 +149,21 @@ void airTripAddLast(struct airTrip* trip, char* name, float longitude, float lat
 } 
 
 void airTripAddBest(struct airTrip* trip, char* name, float longitude, float latitude) {
+    /*
+    Consigna: Agrega un nuevo airport en el mejor lugar que se pueda del recorrido. 
+    Dejando el primer lugar como fijo, se debe buscar el lugar donde agregar la nueva 
+    parada que minimice el recorrido total, es decir, que agregue la menor cantidad de 
+    distancia por tener que pasar por esta nueva parada. En los ejemplos a continuación, 
+    se puede ver como en el primer caso el mejor lugar para agregar la parada es al final, cambiando 
+    el último lugar a visitar. En el segundo caso, lo mejor es que la nueva parada termine entre las únicas 
+    dos paradas de la lista. En el u ́ltimo caso, lo mejor deber ́ıa ser cambiar el primero, pero esto 
+    no se permite. Solo se permite buscar el mejor lugar a partir del primero. Por lo tanto, esta nueva 
+    parada, se termina agregando en el segundo lugar.
 
-    // COMPLETAR
+    Desarrollo:
+    El algoritmo que pensamos es el siguiente:
+    1. Reservamos memoria para el nuevo aeropuerto.
+    */
 
 }
 
